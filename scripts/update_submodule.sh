f454b733ecf7397c6942bc7f03752f55e198b5a9
Hunter Ducharme <hgducharme@gmail.com>
2024-08-23 14:43:15 -0700

    updated script

--- scripts/update_submodule.sh
+++ scripts/update_submodule.sh
@@ -2,8 +2,6 @@
 
 # This script will iterate through all branches in the main repository and will update the branch to point to the latest version of the submodule.
 
-path_to_submodule="../components"
-
 # Store the current branch name
 current_branch=$(git branch --show-current)
 
@@ -12,16 +10,17 @@ git fetch --all
 
 # Loop through each branch
 for branch in $(git branch -r | grep -v '\->' | sed 's/origin\///'); do
-    echo "\nUpdating submodule in branch $branch..."
+    echo
+    echo "Updating submodule in branch $branch..."
 
     # Checkout the branch
     git checkout $branch
 
     # Update the submodule reference
-    git submodule update --remote --merge
+    git submodule update
 
     # Stage the updated submodule reference
-    git add $path_to_submodule
+    git add components
 
     # Commit the change
     git commit -m "Updated submodule in branch $branch"
